aa8fb3df35ea67b57ba36ab105847f2fd44e5188
Egor Pugin <egor.pugin@gmail.com>
2018-08-03 13:45:31 +0300

    Add gpg example.

---
+++ gpg/gpg.cpp
@@ -0,0 +1,50 @@
+/*
+c++: 17
+dependencies:
+    - pvt.cppan.demo.gnu.gpg.gpgme.gpgmecpp: 1
+    - pvt.egorpugin.primitives.sw.main: master
+*/
+
+#include <gpgme.h>
+#include <primitives/sw/main.h>
+
+int main(int argc, char **argv)
+{
+    gpgme_set_global_flag("w32-inst-dir", ".");
+
+    gpgme_check_version("1.11.1");
+
+    //gpgme_ctx_t ctx;
+    //auto err = gpgme_new(&ctx);
+    //err = gpgme_op_keylist_start(ctx, "*", 0);
+
+
+    gpgme_ctx_t ctx;
+    gpgme_key_t key;
+    gpgme_error_t err = gpgme_new(&ctx);
+
+    if (!err)
+    {
+        err = gpgme_op_keylist_start(ctx, "", 0);
+        while (!err)
+        {
+            err = gpgme_op_keylist_next(ctx, &key);
+            if (err)
+                break;
+            printf("%s:", key->subkeys->keyid);
+            if (key->uids && key->uids->name)
+                printf(" %s", key->uids->name);
+            if (key->uids && key->uids->email)
+                printf(" <%s>", key->uids->email);
+            putchar('\n');
+            gpgme_key_release(key);
+        }
+        gpgme_release(ctx);
+    }
+    if (gpg_err_code(err) != GPG_ERR_EOF)
+    {
+        fprintf(stderr, "can not list keys: %s\n", gpgme_strerror(err));
+    }
+
+    return 0;
+}
--- grpc/greeter_server.cc
+++ grpc/greeter_server.cc
@@ -21,52 +21,69 @@
 #include <string>
 
 #include <grpcpp/grpcpp.h>
+#include <primitives/filesystem.h>
 
-#ifdef BAZEL_BUILD
-#include "examples/protos/helloworld.grpc.pb.h"
-#else
 #include "helloworld.grpc.pb.h"
-#endif
 
 using grpc::Server;
 using grpc::ServerBuilder;
 using grpc::ServerContext;
 using grpc::Status;
-using helloworld::HelloRequest;
-using helloworld::HelloReply;
 using helloworld::Greeter;
+using helloworld::HelloReply;
+using helloworld::HelloRequest;
 
 // Logic and data behind the server's behavior.
-class GreeterServiceImpl final : public Greeter::Service {
-  Status SayHello(ServerContext* context, const HelloRequest* request,
-                  HelloReply* reply) override {
-    std::string prefix("Hello ");
-    reply->set_message(prefix + request->name());
-    return Status::OK;
-  }
+class GreeterServiceImpl final : public Greeter::Service
+{
+    Status SayHello(ServerContext *context, const HelloRequest *request,
+                    HelloReply *reply) override
+    {
+        for (auto &[k, v] : context->client_metadata())
+        {
+            std::cout << k << ": " << v << "\n";
+        }
+        std::cout << "\n";
+        auto i = context->client_metadata().find("token");
+        if (i != context->client_metadata().end())
+        {
+            std::cout << "pass: " << i->second << "\n";
+            if (i->second != "pass")
+                return Status(grpc::StatusCode::UNAUTHENTICATED, "bad token");
+        }
+        std::string prefix("Hello ");
+        reply->set_message(prefix + request->name());
+        return Status::OK;
+    }
 };
 
-void RunServer() {
-  std::string server_address("0.0.0.0:50051");
-  GreeterServiceImpl service;
+void RunServer()
+{
+    std::string server_address("localhost:50051");
+    GreeterServiceImpl service;
 
-  ServerBuilder builder;
-  // Listen on the given address without any authentication mechanism.
-  builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
-  // Register "service" as the instance through which we'll communicate with
-  // clients. In this case it corresponds to an *synchronous* service.
-  builder.RegisterService(&service);
-  // Finally assemble the server.
-  std::unique_ptr<Server> server(builder.BuildAndStart());
-  std::cout << "Server listening on " << server_address << std::endl;
+    grpc::SslServerCredentialsOptions ssl_options;
+    ssl_options.pem_key_cert_pairs.push_back({ read_file("server.key"), read_file("server.crt") });
 
-  // Wait for the server to shutdown. Note that some other thread must be
-  // responsible for shutting down the server for this call to ever return.
-  server->Wait();
+    ServerBuilder builder;
+    // Listen on the given address without any authentication mechanism.
+    // builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
+    builder.AddListeningPort(server_address, grpc::SslServerCredentials(ssl_options));
+    // Register "service" as the instance through which we'll communicate with
+    // clients. In this case it corresponds to an *synchronous* service.
+    builder.RegisterService(&service);
+    // Finally assemble the server.
+    std::unique_ptr<Server> server(builder.BuildAndStart());
+    std::cout << "Server listening on " << server_address << std::endl;
+
+    // Wait for the server to shutdown. Note that some other thread must be
+    // responsible for shutting down the server for this call to ever return.
+    server->Wait();
 }
 
-int main(int argc, char** argv) {
-  RunServer();
+int main(int argc, char **argv)
+{
+    RunServer();
 
-  return 0;
+    return 0;
 }
